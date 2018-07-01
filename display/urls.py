9f9e2164aff47a0f8b3384d9f2708bd0129f5085
Mateusz Kraiński <mt.krainski@gmail.com>
2018-07-01 23:50:35 +0300

    cleaned

--- display/urls.py
+++ display/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from . import views
 
+app_name = "display"
 urlpatterns = [
-    path("", views.index, name="index")
+    # /display/
+    path("", views.index, name="index"),
+
+    # /display/4
+    path("<int:question_id>/", views.details, name="details"),
+
+    # /display/4/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+
+    # /display/4/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
--- display/views.py
+++ display/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # template = loader.get_template('display/index.html')
+    # context = {
+    #     'latest_question_list' : latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    return render(
+        request,
+        'display/index.html',
+        {'latest_question_list': latest_question_list}
+    )
+
+
+def details(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(
+        request,
+        'display/detail.html',
+        {'question': question}
+    )
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results for question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting for question {question_id}")
+
